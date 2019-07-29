63040f92c038e2b123f66c350df9e11c2852b486
Matthew Parker <cadburylion@outlook.com>
2019-07-28 19:27:41 -0700

    [BUG FIX]: Attempt to fix bug where entering entire user's route to url fails to load the app

--- backend/server/index.js
+++ backend/server/index.js
@@ -114,7 +114,7 @@ app.get('/:hash', (req, res) => {
   });
 });
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../../build', 'index.html'));
 });
 
